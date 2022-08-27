bcfdf992fe17443ec3a70a90408071ca5e1e7f65
SRIRAM <39412736+messiram@users.noreply.github.com>
2022-08-27 11:40:04 +0530

    User authentication practice project 3

---
+++ UserAuthentication/src/main/java/com/simplilearn/demo/AuthenticationUser.java
@@ -0,0 +1,16 @@
+package com.simplilearn.demo;
+
+public class AuthenticationUser {
+	public String username()
+	{
+		String email = "master@gmail.com";
+		return email;
+	}
+	
+	public String paswd()
+	{
+		String password = "master";
+		return password;
+	}
+
+}
